4619364d7e15690ad025bd6215d9c96b5f6c8c84
herjy <remyjoseph@ymail.com>
2021-08-26 22:55:06 +0200

    new functions

--- Cutout_funct.py
+++ Cutout_funct.py
@@ -20,6 +20,56 @@ def mk_hst_cutout(filename, pos, size, path = ''):
 
     hdu = fits.PrimaryHDU(cut.data, cut.wcs.to_header())
     hdul = fits.HDUList([hdu])
-    hdul.writeto(path+'cut'+filename, clobber=True)
+    hdul.writeto(path+'cut_'+filename, clobber=True)
     
     return cut
+
+def mk_hsc_cutout(filenames, pos=None, size=None, path = '', patch = True):
+    '''Make a (`size`)-large cutout from a file (`filename`) at position (`pos`).
+    '''
+    cube = []
+    if path == False:
+        assert pos is not None and size is not None
+    for f in filenames:
+        hdus = fits.open(path+f)
+        wcs = WCS(hdus[1].header)
+        img = hdus[1].data
+        ny, nx = img.shape
+        if patch == True:
+            if size is None:
+                n = np.min([nx,ny])
+            else:
+                n = size
+            if pos is None:
+                pos = np.array([nx/2, ny/2]).astype(int)
+        else:
+            #TODO: implement wcs extraction
+            pass
+        print(n)
+        cut = Cutout2D(img, pos, (n,n) , wcs = wcs)
+        cube.append(cut.data)
+
+    hdu = fits.PrimaryHDU(np.array(cube), cut.wcs.to_header())
+    hdul = fits.HDUList([hdu])
+    hdul.writeto(path+'cube_HSC_'+f, clobber=True)
+    
+    return cube
+
+
+def mk_psf_cube(filenames, path = ''):
+    '''Make a psf cube out of several files.
+    '''
+    cube = []
+
+    for f in filenames:
+        hdus = fits.open(path+f)
+        wcs = WCS(hdus[0].header)
+        img = hdus[0].data
+
+        cube.append(img)
+
+    hdu = fits.PrimaryHDU(np.array(cube), cut.wcs.to_header())
+    hdul = fits.HDUList([hdu])
+    hdul.writeto(path+'cube_HSC_'+f, clobber=True)
+    
+    return cube
